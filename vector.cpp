506a39f49ec124ff58e618ff3c99a0c2d9fce1db
SidM9 <43111742+Sidm9@users.noreply.github.com>
2020-09-27 11:47:30 +0530

    init

---
+++ vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std
+for (int i = 0; i < count; i++)
+{
+    vector<int> g1;
+    for (int i = 0; i < 5; i++) {
+        g1.pushBack(i);
+        
+    }
+}
